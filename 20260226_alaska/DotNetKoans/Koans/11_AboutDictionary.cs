6518d145d9b43c3b753e168628e16eeb1198638c
Alex Berger <alexbigkid@users.noreply.github.com>
2026-02-25 23:01:40 -0800

    . d add .NET C# Koans

---
+++ 20260226_alaska/DotNetKoans/Koans/11_AboutDictionary.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DotNetKoans.Engine;
+using Xunit;
+
+namespace DotNetKoans.Koans;
+
+public class AboutDictionary : Koan
+{
+	// A dictionary is a C# class.
+	[Step(1)]
+	public void DictionaryIsACSharpClass()
+	{
+		var dict = new Dictionary<string, string>();
+		dict.Add("Value", "Key");
+		var firstElement = dict.First();
+
+		Assert.Equal("Value", firstElement.Key); // Key
+		Assert.Equal("Key", firstElement.Value); // Value
+	}
+
+	//Pass keys to get their values.
+	[Step(2)]
+	public void UsingDictionaryKeysToGetValues()
+	{
+		var dict = new Dictionary<string, string>();
+		dict.Add("Bruce", "Wayne");
+		dict.Add("United Kingdom", "London");
+		dict.Add("Poland", "Warsaw");
+		dict.Add("Japan", "Tokyo");
+
+		var key = "Japan";
+		Assert.Equal("Tokyo", dict[key]); // What is the value?
+	}
+
+	//Check if a key exists in Dictionary.
+	[Step(3)]
+	public void CheckIfKeyExists()
+	{
+		var dict = new Dictionary<string, string>();
+		dict.Add("Bruce", "Wayne");
+		dict.Add("United Kingdom", "London");
+		dict.Add("Poland", "Warsaw");
+		dict.Add("Japan", "Tokyo");
+
+		var key = "Bruce";
+		Assert.True(true, dict.ContainsKey(key).ToString()); // How to make this statement true?
+	}
+
+	//Check if a value exists in Dictionary.
+	[Step(4)]
+	public void CheckIfValueExists()
+	{
+		var dict = new Dictionary<string, string>();
+		dict.Add("Bruce", "Wayne");
+		dict.Add("United Kingdom", "London");
+		dict.Add("Poland", "Warsaw");
+		dict.Add("Japan", "Tokyo");
+
+		var val = "Wayne";
+		Assert.True(true, dict.ContainsValue(val).ToString()); // How to make this statement true?
+	}
+
+	//Update the value of a key in dictionary.
+	[Step(5)]
+	public void UpdateValueOfKey()
+	{
+		var dict = new Dictionary<string, string>();
+		dict.Add("Bruce", "Wayne");
+		dict.Add("United Kingdom", "London");
+		dict.Add("Poland", "Warsaw");
+		dict.Add("Japan", "Tokyo");
+		dict.Add("India", "Mumbai");
+
+		var key = "India";
+		var expectedValue = "New Delhi";
+
+		//May be you should update this
+		dict[key] = "New Delhi";
+
+		Assert.Equal(expectedValue, dict[key]); // How to make this statement true?
+	}
+
+	//Remove a key from dictionary and check its value.
+	[Step(6)]
+	public void RemoveKeyAndCheckIfItExists()
+	{
+		var dict = new Dictionary<string, string>();
+		dict.Add("Bruce", "Wayne");
+		dict.Add("United Kingdom", "London");
+		dict.Add("Poland", "Warsaw");
+		dict.Add("Japan", "Tokyo");
+		dict.Add("India", "Mumbai");
+
+		var keyToRemove = "Bruce";
+
+		if (dict.ContainsKey(keyToRemove))
+			dict.Remove(keyToRemove);
+
+		Assert.False(dict.ContainsKey(keyToRemove)); // How to make this statement true?
+	}
+
+}
---
+++ 20260226_alaska/DotNetKoans/Koans/7_AboutConstants.cs
@@ -0,0 +1,119 @@
+using DotNetKoans.Engine;
+using Xunit;
+
+namespace DotNetKoans.Koans;
+
+public class AboutConstants : Koan
+{
+	[Step(1)]
+	public void ConstantsMustBeInitalizedAsDeclared()
+	{
+		const int months = 12;
+		Assert.Equal(months, 12);
+	}
+
+	[Step(2)]
+	public void ConstantsCannotBeChanged()
+	{
+		//Since C# inserts literal values into compiled
+		//code, you will not achieve zen when attempting
+		//to change them after definition.
+		const int days = 365;
+		// days = days + 1; // does not compile
+		Assert.Equal(days, 365);
+	}
+
+	[Step(3)]
+	public void ConstantsOfTheSameTypeCanBeDeclaredAtTheSameTime()
+	{
+		//You can achieve zen (and save keystrokes) by defining
+		//constants of the same type as one.
+		const int months = 12, weeks = 52, days = 365;
+		Assert.Equal(typeof(int), months.GetType());
+		Assert.Equal(typeof(int), weeks.GetType());
+		Assert.Equal(typeof(int), days.GetType());
+	}
+
+	[Step(4)]
+	public void ConstantsCanBeUsedInExpressionsToInitializeOtherConstants()
+	{
+		const int months = 12;
+		const int weeks = 52;
+		const int days = 365;
+
+		const double daysPerWeek = (double)days / (double)weeks;
+		const double daysPerMonth = (double)days / (double)months;
+		Assert.Equal(365d / 52d, daysPerWeek);
+		Assert.Equal(365d / 12d, daysPerMonth);
+
+		//Constants can be used in arithmetic to set other constant values.
+		//They can also initialize each other.
+	}
+
+	class Animal
+	{
+		public const int Legs = 4;
+
+		public int LegsInAnimal()
+		{
+			return Legs;
+		}
+
+		public class NestedAnimal
+		{
+			public int LegsInNestedAnimal()
+			{
+				return Legs;
+			}
+		}
+	}
+
+	[Step(5)]
+	public void NestedClassesInheritConstantsFromEnclosingClasses()
+	{
+		var nestedAnimal = new Animal.NestedAnimal();
+		Assert.Equal(4, nestedAnimal.LegsInNestedAnimal());
+
+		//QUESTION: Do nested classes inherit their parent's scope?
+	}
+
+	class Reptile : Animal
+	{
+		public int LegsInReptile()
+		{
+			return Legs;
+		}
+	}
+
+	[Step(6)]
+	public void SubclassesInheritConstantsFromParentClasses()
+	{
+		//If a Reptile is an Animal, zen is achieved
+		//when you realize they too will have legs.
+		var reptile = new Reptile();
+		Assert.Equal(4, reptile.LegsInReptile());
+	}
+
+	class MyAnimals
+	{
+		public const int Legs = 2;
+
+		public class Bird : Animal
+		{
+			public int LegsInBird()
+			{
+				return Legs;
+			}
+		}
+	}
+
+	[Step(7)]
+	public void WhoWinsWithBothNestedAndInheritedConstants()
+	{
+		var bird = new MyAnimals.Bird();
+		Assert.Equal(4, bird.LegsInBird());
+
+		/* QUESTION: Which has precedence: The constant in the lexical scope,
+		   or the constant from the inheritance hierarchy? */
+	}
+}
