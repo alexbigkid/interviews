fec0ef350c4c87a591f56fa27eb6968785c05043
Alex Berger <alexbigkid@users.noreply.github.com>
2026-02-26 15:53:34 -0800

    . d finish Directory koans

--- 20260226_alaska/DotNetKoans/Koans/21_AboutDirectory.cs
+++ 20260226_alaska/DotNetKoans/Koans/21_AboutDirectory.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using DotNetKoans.Engine;
 using Xunit;
@@ -5,37 +6,48 @@ using IOPath = System.IO.Path;
 
 namespace DotNetKoans.Koans;
 
-public class AboutDirectory : Koan
+public class AboutDirectory : Koan, IDisposable
 {
-	//Directory is a class that provides static methods for creating, moving, and enumerating through directories and subdirectories.
-
-	private static string directoryName = "temp directory";       
+	private static string directoryName = "temp directory";
 	private static string fullPath = IOPath.Combine(IOPath.GetTempPath(), directoryName); // GetTempPath() Returns the path of the current user's temporary folder.
+
+    // constructor runs before each test
+    public AboutDirectory()
+    {
+        if (Directory.Exists(fullPath))
+            Directory.Delete(fullPath, true);
+    }
+
+      // runs after each test
+    public void Dispose()
+    {
+        if (Directory.Exists(fullPath))
+            Directory.Delete(fullPath, true);
+    }
+
+	//Directory is a class that provides static methods for creating, moving, and enumerating through directories and subdirectories.
 
 	[Step(1)]
 	public void CreatingAndDeletingDirectory()
-	{            
+	{
 		Directory.CreateDirectory(fullPath);
-
-		Assert.Equal(FILL_ME_IN, Directory.Exists(fullPath));
-
+		Assert.Equal(true, Directory.Exists(fullPath));
 		Directory.Delete(fullPath);
-
-		Assert.Equal(FILL_ME_IN, Directory.Exists(fullPath));
+		Assert.Equal(false, Directory.Exists(fullPath));
 	}
-        
+
 	[Step(2)]
 	public void GetDirectoryInfo()
 	{
 		var directoryInfo = new DirectoryInfo(fullPath);
 		directoryInfo.Create();
 
-		Assert.Equal(FILL_ME_IN, directoryInfo.Exists);
-		Assert.Equal(FILL_ME_IN, directoryInfo.Name);
+		Assert.Equal(true, directoryInfo.Exists);
+		Assert.Equal(directoryName, directoryInfo.Name);
 
 		directoryInfo.Delete(false);
 	}
-        
+
 	[Step(3)]
 	public void CreateSubDirectory()
 	{
@@ -44,26 +56,22 @@ public class AboutDirectory : Koan
 		directoryInfo.CreateSubdirectory("subdirectory1");
 		directoryInfo.CreateSubdirectory("subdirectory2");
 
-		Assert.Equal(FILL_ME_IN, directoryInfo.GetDirectories().Length); // what is the number of subdirectories?
+		Assert.Equal(2, directoryInfo.GetDirectories().Length); // what is the number of subdirectories?
 
 		directoryInfo.Delete(true);
 	}
-        
+
 	[Step(4)]
 	public void GetFilesInDirectory()
 	{
 		var directoryInfo = new DirectoryInfo(fullPath);
 		directoryInfo.Create();
-            
+
 		using (File.Create(IOPath.Combine(fullPath, "file1")))
 		using (File.Create(IOPath.Combine(fullPath, "file2")))
-		{
-		}
 
-		Assert.Equal(FILL_ME_IN, directoryInfo.GetFiles().Length); // what is the number of files that exist in this directory?
+		Assert.Equal(2, directoryInfo.GetFiles().Length); // what is the number of files that exist in this directory?
 
 		directoryInfo.Delete(true);
-
 	}
-
 }
