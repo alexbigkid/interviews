1751965f8ed74c4b4448590478208adb4bd5fc69
Alex Berger <alexbigkid@users.noreply.github.com>
2026-02-26 16:21:23 -0800

    . d finish linq koans

--- 20260226_alaska/DotNetKoans/Koans/23_AboutLinq.cs
+++ 20260226_alaska/DotNetKoans/Koans/23_AboutLinq.cs
@@ -10,11 +10,8 @@ public class AboutLinq : Koan
 	 *LINQ (Language Integrated Query).
 	 * LINQ provides language-level querying capabilities and a higher-order function API to C# and VB as a way to write expressive, declarative code.
 	 * All LINQ query operations consist of three distinct actions:
-
 	    1. Obtain the data source.
-
 	    2. Create the query.
-
 	    3. Execute the query.
 	 */
 
@@ -22,9 +19,7 @@ public class AboutLinq : Koan
 	public void FilterArrayData()
 	{
 		//This sample uses "where" to find all elements of an array less than 5.
-
 		// The Three Parts of a LINQ Query:
-
 		//  1. Data source.
 		int[] numbers = { 5, 1, 9, 8, 6, 7 };
 
@@ -35,7 +30,7 @@ public class AboutLinq : Koan
 			select n;
 
 		// 3. Query execution.
-		Assert.Equal(FILL_ME_IN, lowNums.Count());
+		Assert.Equal(1, lowNums.Count());
 	}
 
 	[Step(2)]
@@ -48,19 +43,16 @@ public class AboutLinq : Koan
 			orderby cust ascending //You can also use descending here for reverse order.
 			select cust;
 
-		Assert.Equal(FILL_ME_IN, orderedCustomers.First());
-		Assert.Equal(FILL_ME_IN, orderedCustomers.Last());
+		Assert.Equal("Anna", orderedCustomers.First());
+		Assert.Equal("Maria", orderedCustomers.Last());
 	}
 
 	[Step(3)]
 	public void GetJustTheDataYouWantUsingTake()
 	{
 		int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
-
-		//Get just the 3 first numbers.
-		var first3Numbers = numbers.Take(3);
-
-		Assert.Equal(FILL_ME_IN, first3Numbers.Count());
+		var first3Numbers = numbers.Take(3); //Get just the 3 first numbers.
+		Assert.Equal(3, first3Numbers.Count());
 	}
 
 	[Step(4)]
@@ -69,61 +61,53 @@ public class AboutLinq : Koan
 		string[] words = { "believe", "relief", "receipt", "field" };
 
 		bool iAfterE = words.Any(w => w.Contains("ei")); //Check if any of your words contain 'ei'
-		Assert.Equal(FILL_ME_IN, iAfterE);
+		Assert.Equal(true, iAfterE);
 	}
 
-        
+
 	[Step(5)]
 	public void HowToUseWhereToFilterData()
 	{
 		var numbers = new[] {1, 2, 3, 4};
 		var result = numbers.Where(x => x > 2).ToArray();
-			
 		//What values should be in array?
-		Assert.Equal(FILL_ME_IN, result);
+		Assert.Equal([3, 4], result);
 	}
-		
+
 	[Step(6)]
 	public void HowToGetInfoIfValueIsGreaterThanUsingSelect()
 	{
 		var numbers = new[] {1, 2, 3, 4};
 		var result = numbers.Select(x => x > 2).ToArray();
-			
 		//What values should be in array?
-		Assert.Equal(FILL_ME_IN, result);
+		Assert.Equal([ false, false, true, true ], result);
 	}
 
 	[Step(7)]
 	public void GetSumOfTheData()
 	{
 		int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
-
 		//Get sum of the array.
 		var sum = numbers.Sum();
-
-		Assert.Equal(FILL_ME_IN, sum);
+		Assert.Equal(45, sum);
 	}
 
 	[Step(8)]
 	public void GetMinimumOfTheData()
 	{
 		int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
-
 		//Get minimum of the array.
 		var min = numbers.Min();
-
-		Assert.Equal(FILL_ME_IN, min);
+		Assert.Equal(0, min);
 	}
 
 	[Step(9)]
 	public void GetMaximumOfTheData()
 	{
 		int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
-
 		//Get maximum of the array.
 		var max = numbers.Max();
-
-		Assert.Equal(FILL_ME_IN, max);
+		Assert.Equal(9, max);
 	}
 
 
@@ -131,10 +115,8 @@ public class AboutLinq : Koan
 	public void GetAverageOfTheData()
 	{
 		int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
-
 		//Get average of the array.
 		var average = numbers.Average();
-
-		Assert.Equal(FILL_ME_IN, average);
+		Assert.Equal(4.5, average);
 	}
 }
